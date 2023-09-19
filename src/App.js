94d951a0b5c81c3e47dd53ca60cd865612d6dd21
syedshams111111 <panoaqil2000@gmail.com>
2023-09-20 00:01:52 +0500

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Header from "./components/header/Header.js";
+import Search from "./components/search/Search.js";
+import AddProducts from "./components/addproducts/AddProducts.js";
+import CardBody from "./components/cards/CardBody.js";
+import Button from "./components/button/Button.js";
 
-function App() {
+import "./App.css";
+const App = () => {
+  const [items, setItem] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [addedItems, setAddedItem] = useState([]);
+  const [showAddProducts, setShowAddProducts] = useState(false);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/")
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+    console.count("hi");
+  }, []);
+  function changingSrarchData(e) {
+    setSearchValue(e.target.value);
+  }
+  const itmesFilter = items.filter((item) =>
+    item.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  function addItem(item) {
+    item.addNumber = 1;
+    const itemArr = addedItems;
+    setAddedItem([...itemArr, item]);
+  }
+  // console.log(addedItems);
+  function removeItem(item) {
+    const newItems = addedItems.filter((addedItem) => addedItem.id !== item.id);
+    setAddedItem(newItems);
+    // console.log(addedItems);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Header /> */}
+
+      <div className="body__container">
+        <div className="nav">
+          <Header />
+          <div className="nav-right">
+            <Search
+              products={items}
+              value={searchValue}
+              onChangeData={changingSrarchData}
+            />
+            <Button num={addedItems.length} click={setShowAddProducts} />
+          </div>
+        </div>
+
+        {showAddProducts && (
+          <AddProducts
+            click={setShowAddProducts}
+            items={addedItems}
+            removeItem={removeItem}
+            setAddedItem={setAddedItem}
+          />
+        )}
+        <CardBody
+          products={itmesFilter}
+          addItem={addItem}
+          removeItem={removeItem}
+          addedItems={addedItems}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
